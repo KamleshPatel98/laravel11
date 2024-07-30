0157b090806ed6904f57a7bc7435612c2731e75e
KamleshPatel98 <kamleshp52170@gmail.com>
2024-07-30 10:05:06 +0530

    api jwt authentication setup

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CaptchaController;
+use Illuminate\Support\Facades\Hash;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return Hash::make(12345678);
 });
+
 
 Route::get('captcha-form',[CaptchaController::class,'captcha_form'])->name('captcha_form');
 Route::get('get-captcha',[CaptchaController::class,'get_captcha'])->name('get_captcha');
