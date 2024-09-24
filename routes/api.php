e0f5c7bc35b8f47dce07f0a406a804df67c5a0fa
KamleshPatel98 <kamleshp52170@gmail.com>
2024-09-24 18:44:56 +0530

    controller under api directory

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\FetchController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,3 +21,5 @@ Route::middleware(['auth:api'])->group(function () {
     Route::post('refresh', [AuthController::class,'refresh']);
     Route::post('me', [AuthController::class,'me']);
 });
+
+Route::get('test',[FetchController::class,'test']);
